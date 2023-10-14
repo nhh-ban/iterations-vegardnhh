03ac7414b550b69569a064675b9452314d558290
Vegard Sorheim <vegard.sorheim@student.nhh.no>
2023-10-14 19:41:37 +0200

    Problem 3, found problem with UTC, correct now

--- functions/data_transformations.R
+++ functions/data_transformations.R
@@ -6,7 +6,8 @@ transform_metadata_to_df <- function(stations_metadata) {
     map(as_tibble) %>%  
     list_rbind() %>%  
     mutate(latestData = map_chr(latestData, 1, .default = "")) %>%  
-    mutate(latestData = as_datetime(latestData, tz = "Europe/Berlin")) %>% 
+    mutate(latestData = as_datetime(latestData)) %>% 
+    mutate(latestData = force_tz(latestData, tzone = "UTC")) %>%
     mutate(location = map(location, unlist)) %>%   
     mutate(
       lat = map_dbl(location, "latLon.lat"),
